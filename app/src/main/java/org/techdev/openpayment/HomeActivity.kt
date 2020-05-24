4e769d0243a726432bbb488227a3bf94625f609c
Braian <braiancoronelosmar@gmail.com>
2020-05-24 04:43:52 -0300

    Add payment list fragment

--- app/src/main/java/org/techdev/openpayment/HomeActivity.kt
+++ app/src/main/java/org/techdev/openpayment/HomeActivity.kt
@@ -2,21 +2,42 @@ package org.techdev.openpayment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.fragment.app.Fragment
 import org.techdev.openpayment.di.HomeModule
 import org.techdev.openpayment.extensions.app
+import org.techdev.openpayment.payment.ui.PaymentFragment
+import org.techdev.openpayment.payment.ui.dummy.DummyContent
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), PaymentFragment.OnListFragmentInteractionListener {
 
 //    Subcomponente
     val homeComponent by lazy {
         app.appComponent.plus(HomeModule(this))
     }
 
+    private var paymentFragment: PaymentFragment? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         homeComponent.inject(this)
-        Log.d("Test", "Hi")
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.payment_methods_container, paymentFragmentProvider())
+            .commit()
+
     }
+
+    private fun paymentFragmentProvider(): Fragment {
+        if (paymentFragment == null) {
+            paymentFragment = PaymentFragment()
+        }
+        return paymentFragment as PaymentFragment
+    }
+
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
+
+    }
+
 }
